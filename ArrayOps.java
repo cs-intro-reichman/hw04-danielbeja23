f52ba17d652d29d967284c4dd482ae8589e87e71
daniel beja <daniel.beja@post.runi.ac.il>
2024-01-16 19:22:32 +0200

    first

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,24 +1,31 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int[] array = new int[] { 0 };
+        System.out.println(findMissingInt(array));
+
     }
 
-    public static int secondMaxValue(int [] array) {
+    public static int findMissingInt(int[] array) {
+        int sum = array.length; // sum of 0 to N.
+        int sumOfArray = 0;// sum of the values of the array.
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + i;
+            sumOfArray = sumOfArray + array[i];
+        }
+        return (sum - sumOfArray);
+    }
+
+    public static int secondMaxValue(int[] array) {
         // Write your code here:
         return 0;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
         // Write your code here:
         return false;
     }
 
-    public static boolean isSorted(int [] array) {
+    public static boolean isSorted(int[] array) {
         // Write your code here:
         return false;
     }
